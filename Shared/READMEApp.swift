c81e79b15d01156c2fe180a5951e9894f3597305
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-06-30 13:43:28 +0530

    Initial Commit

---
+++ Shared/READMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  READMEApp.swift
+//  Shared
+//
+//  Created by Shreya Prasad on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct READMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
